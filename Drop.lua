ea1ba290572f4dba65d37f00686ae1a11d331a64
zuqorl <alextheo482@gmail.com>
2025-03-02 17:43:45 +0100

    Update Drop.lua

--- Drop.lua
+++ Drop.lua
@@ -1,3 +1,5 @@
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/zuqorl/CDTStormb-ta/refs/heads/main/Ui.lua"))():MakePrototypeLibrary("CDTStorm bêta")
 
 local CuteTab = Lib:MakeTab("Information", true) -- true to make it open by default (optional)
+
+local Section = Tab:CreateSection("Section Example")
