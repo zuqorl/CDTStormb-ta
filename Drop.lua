c0d159d213759d0b761d03a6fc7cb87cb84ddaf1
zuqorl <alextheo482@gmail.com>
2025-03-02 18:02:04 +0100

    Update Drop.lua

--- Drop.lua
+++ Drop.lua
@@ -4,3 +4,26 @@ local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/zuqorl/CD
 local CuteTab = Lib:MakeTab("Information", true) -- true to make it open by default (optional)
 
 local CuteTab = Lib:CreateSection("Section Example")
+https://github.com/OopssSorry/LuaU-Free-Key-System-UI
+local KeySystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/LuaU-Free-Key-System-UI/main/source.lua"))()
+local KeyValid = false
+local response = KeySystem:Init({
+	Debug=false, -- <bool> Prints some output in console when true
+	Title="ExampleHub | Key System", -- <string or nil> Title of key system
+	Description=nil, -- <string or nil> Description of key system
+	Link="", -- <string> String to get key
+	Discord="test", -- <string or nil> Button to join your discord server
+	SaveKey=false, -- <bool or nil> Just auto save key
+	Verify=function(key) -- <function> Verify is key valid
+		if key=="1234" then
+      KeyValid=true
+			return true
+		else
+			return false
+		end
+	end,
+	GuiParent = game.CoreGui, -- <object or nil> :3
+})
+
+if not response or not KeyValid then return end
+-- HERE YOUR SCRIPT
