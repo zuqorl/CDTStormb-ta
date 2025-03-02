0a8d29a69a546b56f4195e309268b67058469026
zuqorl <alextheo482@gmail.com>
2025-03-02 18:09:37 +0100

    Update Keysystem.lua

--- Keysystem.lua
+++ Keysystem.lua
@@ -3,10 +3,10 @@ local KeySystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/Oop
 local KeyValid = false
 local response = KeySystem:Init({
 	Debug=false, -- <bool> Prints some output in console when true
-	Title="Test", -- <string or nil> Title of key system
-	Description=nil, -- <string or nil> Description of key system
-	Link="", -- <string> String to get key
-	Discord="test", -- <string or nil> Button to join your discord server
+	Title="CDTStorm bêta", -- <string or nil> Title of key system
+	Description="Hi and welcome to CDTStorm bêta", -- <string or nil> Description of key system
+	Link="https://discord.gg/c5dePr9f", -- <string> String to get key
+	Discord="https://discord.gg/c5dePr9f", -- <string or nil> Button to join your discord server
 	SaveKey=false, -- <bool or nil> Just auto save key
 	Verify=function(key) -- <function> Verify is key valid
 		if key=="1234" then
