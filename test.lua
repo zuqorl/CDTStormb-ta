8ba8f17c73e23bf41f911f8ebec1900e130087eb
zuqorl <alextheo482@gmail.com>
2025-03-02 22:36:46 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -38,13 +38,34 @@ tab1.newButton("Button", "Prints Hello!", function(state)
   end
   end)
 
-tab1.newToggle("Toggle", "Toggle! (prints the state)", true, function(toggleState)
-    if toggleState then
-        print("On")
-    else
-        print("Off")
-    end
-end)
+tab1.newToggle("Toggle", "Toggle! (prints the state)", true, function(state)
+  getfenv().auto = (state and true or false)
+  wait(1)
+  workspace.Gravity = getfenv().grav
+  while getfenv().auto do
+    task.wait()
+  local chr = game.Players.LocalPlayer.Character
+  local car = chr.Humanoid.SeatPart.Parent.Parent
+    if not workspace:FindFirstChild("justapart") then
+      local new = Instance.new("Part",workspace)
+      new.Name = "justapart"
+      new.Size = Vector3.new(10000,20,10000)
+      new.Anchored = true
+      new.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.Position+Vector3.new(0,1000,0)
+  end
+  car:PivotTo(workspace:FindFirstChild("justapart").CFrame*CFrame.new(0,7,1000))
+  local pos = workspace:FindFirstChild("justapart").CFrame*CFrame.new(0,7,-1000)
+  repeat task.wait()
+    local speed =  getfenv().speed or 300
+    workspace.Gravity = 500
+            car.PrimaryPart.Velocity = Vector3.new(car.PrimaryPart.Velocity.X,-100,car.PrimaryPart.Velocity.Z) 
+    car:PivotTo(CFrame.new(car.PrimaryPart.Position,Vector3.new(pos.X,car.PrimaryPart.Position.Y,pos.Z)))
+            car.PrimaryPart.Velocity = Vector3.new(car.PrimaryPart.Velocity.X,-100,car.PrimaryPart.Velocity.Z) 
+    car.PrimaryPart.AssemblyLinearVelocity = car.PrimaryPart.CFrame.LookVector*speed
+    car.PrimaryPart.Velocity = Vector3.new(car.PrimaryPart.Velocity.X,-100,car.PrimaryPart.Velocity.Z) 
+  until game.Players.LocalPlayer:DistanceFromCharacter(Vector3.new(pos.X,pos.Y,pos.Z)) < 200 or getfenv().test == false
+  end
+  end)
 tab1.newInput("Input", "Prints your input.", function(text)
     print("Entered text in Tab 1: " .. text)
 end)
