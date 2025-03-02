60fd80fc26668eb183d3d6bd318fb98fec6b4b11
zuqorl <alextheo482@gmail.com>
2025-03-02 18:06:54 +0100

    Update and rename Key system to Keysystem.lua

---
+++ Keysystem.lua
@@ -0,0 +1,23 @@
+
+local KeySystem = loadstring(game:HttpGet("https://raw.githubusercontent.com/OopssSorry/LuaU-Free-Key-System-UI/main/source.lua"))()
+local KeyValid = false
+local response = KeySystem:Init({
+	Debug=false, -- <bool> Prints some output in console when true
+	Title="Test", -- <string or nil> Title of key system
+	Description=nil, -- <string or nil> Description of key system
+	Link="", -- <string> String to get key
+	Discord="test", -- <string or nil> Button to join your discord server
+	SaveKey=false, -- <bool or nil> Just auto save key
+	Verify=function(key) -- <function> Verify is key valid
+		if key=="1234" then
+      KeyValid=true
+			return true
+		else
+			return false
+		end
+	end,
+	GuiParent = game.CoreGui, -- <object or nil> :3
+})
+
+if not response or not KeyValid then return end
+-- HERE YOUR SCRIPT
