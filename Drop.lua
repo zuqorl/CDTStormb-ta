162118139bcf9cb23d8a24a3d4cced9431f1d9d2
zuqorl <alextheo482@gmail.com>
2025-03-02 17:46:28 +0100

    Update Drop.lua

--- Drop.lua
+++ Drop.lua
@@ -1,3 +1,4 @@
+if game.PlaceId == 1554960397 then
 local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/zuqorl/CDTStormb-ta/refs/heads/main/Ui.lua"))():MakePrototypeLibrary("CDTStorm bêta")
 
 local CuteTab = Lib:MakeTab("Information", true) -- true to make it open by default (optional)
